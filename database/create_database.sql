17d311c845739f45b56a7f21b877436457401f6d
Rasa <parsa.rasatalab@gmail.com>
2026-08-25 09:36:59 +0330

    Create Database

---
+++ database/create_database.sql
@@ -0,0 +1,6 @@
+-- Create the local MySQL database for the Global Superstore project.
+-- Run this script before importing the source Superstore.sql file.
+
+CREATE DATABASE IF NOT EXISTS global_superstore;
+
+USE global_superstore;
